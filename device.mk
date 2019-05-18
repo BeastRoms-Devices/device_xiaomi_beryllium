37f0cb4034f2758c38a93b725ae59290853b381f
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-18 10:12:24 +0200

    beryllium: Improve top padding when notch is hidden

--- device.mk
+++ device.mk
@@ -31,9 +31,6 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
-	
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
 
 # Properties
 -include $(LOCAL_PATH)/device-props.mk
